ee10ffb3a94e90053300005c8d66b889f89dbda4
BeckSM64 <becksm64@gmail.com>
2021-04-10 13:25:41 -0400

    :tada: initial commit

---
+++ bot.py
@@ -0,0 +1,61 @@
+# bot.py
+import os
+import discord
+import datetime
+from dotenv import load_dotenv
+from discord.ext import commands
+from discord.ext import tasks
+from discord import Member
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+global babies
+babies = []
+
+@bot.event
+async def on_ready():
+    await babysitLoop.start()
+
+@bot.command()
+async def babysit(ctx, member: discord.Member):
+    global babies
+    babies.append(str(member))
+
+def time_in_range(start, end, x):
+    """Return true if x is in the range [start, end]"""
+    if start <= end:
+        return start <= x <= end
+    else:
+        return start <= x or x <= end
+
+@tasks.loop(seconds=1)
+async def babysitLoop():
+    global babies
+    for guild in bot.guilds:
+        if guild.id == int(GUILD):
+            for member in guild.members:
+
+                # Start and end time that babysitting should occur
+                # Current time for comparison
+                start = datetime.time(12, 0, 0)
+                end = datetime.time(20, 0, 0)
+                now = datetime.datetime.now().time()
+
+                # Find the member that needs to go to bed
+                # TODO: start task from command and pass desired member to babysit from there
+                if (str(member) in babies) and (member.voice is not None) and (time_in_range(start, end, now)):
+                    
+                    # Server deafen the member
+                    await member.edit(mute=True)
+                    await member.edit(deafen=True)
+
+                    # Disconnect the user from any voice channel they're connected to
+                    await member.move_to(channel=None, reason=None)
+
+bot.run(TOKEN)
